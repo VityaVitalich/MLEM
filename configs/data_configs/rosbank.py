063f8c13b6f9f9aa54f81d5ef10f7a17696d1db9
VityaVitalich <vvmoskvoretskiy@yandex.ru>
2023-08-16 12:38:04 +0000

    dataset preparation testing

--- configs/data_configs/rosbank.py
+++ configs/data_configs/rosbank.py
@@ -6,5 +6,32 @@ def data_configs():
 
     # data
     config.train_path = '~/event_seq/experiments/rosbank/data/train_trx.parquet'
+    config.client_list_shuffle_seed = 0xAB0BA #seed for splitting data to train and validation
+    config.valid_size = 0.05 #validation size 
+    config.col_id = 'cl_id' #column defining ids. used for sorting data
+
+    features = config.features = ml_collections.ConfigDict()
+    # dict below should define all the features that are not numeric with names as keys. 
+    # "in" parameter is used to clip values at the input.
+    # have not figured out the purpose of "out"
+    features.embeddings = {
+        "mcc": {"in": 100, "out": 24},
+        "channel_type": {"in": 4, "out": 4},
+        "currency": {"in": 4, "out": 4},
+        "trx_category": {"in": 10, "out": 4}
+      }
+    # all numeric features are defined here as keys
+    # seem like its value is technical and is not used anywhere
+    features.numeric_values = {
+        "amount": "identity"
+      }
+
+    #splitters
+    config.split_strategy = {
+        "split_strategy": "SampleSlices",
+        "split_count": 5,
+        "cnt_min": 15,
+        "cnt_max": 150
+      }
 
     return config
