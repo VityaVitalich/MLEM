61322050977f725b100f9efc3efa14b9c04f584c
moskvoretskijviktor <enemy1307@mail.ru>
2023-08-23 14:33:39 +0300

    testing train loop

--- configs/model_configs/mTAN/rosbank.py
+++ configs/model_configs/mTAN/rosbank.py
@@ -6,15 +6,15 @@ def model_configs():
 
     ########## Embeddings ##############
 
-    config.features_emb_dim = 16
-    config.num_ref_points = 64
-    config.latent_dim = 4
-    config.ref_point_dim = 16
-    config.time_emb_dim = 16
+    config.features_emb_dim = 4
+    config.num_ref_points = 16
+    config.latent_dim = 2
+    config.ref_point_dim = 8
+    config.time_emb_dim = 8
     config.num_heads_enc = 2
     config.linear_hidden_dim = 32
-    config.k_iwae = 5
-    config.noise_std = 0.1
+    config.k_iwae = 1
+    config.noise_std = 0.5
     config.kl_weight = 0.1
     config.device = "cpu"
 
