d57a099381eef2adf3452b3a4fcaa40d8488bdea
xaosina <dima.tina2013@gmail.com>
2023-11-22 14:03:32 +0000

    st

--- experiments/run_pipe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-EPOCHS=1
-DEVICE="cuda"
-NAME='test2'
-N_RUNS=1
-DATASET="rosbank"
-
-python pipeline.py --total-epochs=$EPOCHS --device=$DEVICE --run-name=$NAME --n-runs=$N_RUNS --dataset=$DATASET
